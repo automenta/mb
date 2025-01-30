6a99bca8964f6ab69240a6f36d4b952f51ecd664
me <me@internet.earth>
2025-01-30 16:16:25 -0500

    feat: Implement view switching and initial views

--- ui/app.ts
+++ ui/app.ts
@@ -1,19 +1,17 @@
 import $ from 'jquery';
-import {
-  io,
-  Socket
-} from 'socket.io-client';
+import { io, Socket } from 'socket.io-client';
 import DB from '../core/db';
 import Matching from '../core/match';
 import Network from '../core/net';
-import {
-    getStore, Store
-} from './store';
-import {
-  Awareness
-} from 'y-protocols/awareness';
+import { getStore, Store } from './store';
+import { Awareness } from 'y-protocols/awareness';
 import ViewManager from './view-manager';
-import {SchemaRegistry} from "../core/schema-registry";
+import { SchemaRegistry } from "../core/schema-registry";
+import MeView from "./me.view";
+import DBView from './db.view';
+import NetView from './net.view';
+import MatchView from "./match.view";
+//import {SettingsView} from "./settings.view";
 
 class ThemeManager {
     isDarkMode: boolean;
@@ -35,7 +33,6 @@ class ThemeManager {
         this.applyTheme();
     }
 }
-
 
 export default class App {
     readonly channel: string;
@@ -61,7 +58,6 @@ export default class App {
         this.schemaRegistry = new SchemaRegistry();
 
         this.viewManager = new ViewManager(this);
-        this.viewManager.registerViews();
 
         this.socket = io(); // Initialize socket connection
 
@@ -80,10 +76,6 @@ export default class App {
             this.store.setNetworkStatus('disconnected');
         });
 
-        $('#theme-toggle-button').on('click', () => {
-            this.themeManager.toggleTheme();
-        });
-
         this.render();
         this.viewManager.registerViews();
         console.log('App initialized', this);
@@ -98,14 +90,16 @@ export default class App {
                         <li id="db-view-link" class="active">Database</li>
                         <li id="net-view-link">Network</li>
                         <li id="match-view-link">Matching</li>
-                        <li>Profile</li>
-                        <li>Settings</li>
+                        <li id="profile-view-link">Profile</li>
+                        <li id="settings-view-link">Settings</li>
                     </ul>
                 </div>
                 <div class="main-view">
                     <div id="db-view"></div>
                     <div id="net-view" style="display: none;"></div>
                     <div id="match-view" style="display: none;"></div>
+                    <div id="profile-view" style="display: none;"></div>
+                    <div id="settings-view" style="display: none;"></div>
                 </div>
             </div>
         `;
@@ -113,41 +107,41 @@ export default class App {
     }
 
     private setupViewSwitching(): void {
-        $('#db-view-link').on('click', () => {
-            this.showView('db-view');
-        });
-
-        $('#match-view-link').on('click', () => {
-            this.showView('match-view');
-        });
-
-        $('#net-view-link').on('click', () => {
-            this.showView('net-view');
-        });
-
-        $('#match-view-link').on('click', () => {
-            this.showView('match-view');
-        });
+        $('#db-view-link').on('click', () => this.showView('db-view'));
+        $('#net-view-link').on('click', () => this.showView('net-view'));
+        $('#match-view-link').on('click', () => this.showView('match-view'));
+        $('#profile-view-link').on('click', () => this.showView('profile-view'));
+        $('#settings-view-link').on('click', () => this.showView('settings-view'));
     }
 
     private showView(viewId: string): void {
         $('.main-view > div').hide(); // Hide all views
         $(`#${viewId}`).show(); // Show the selected view
 
-        // Initialize MatchView only when showing the match view
-        if (viewId === 'match-view') {
-            const matchViewRoot = $('#match-view');
-            if (matchViewRoot.length) {
-                new MatchView(matchViewRoot, this.match).render();
-            } else {
-                console.error('Element #match-view not found.');
-            }
+        // Initialize views if they haven't been already
+        switch (viewId) {
+            case 'db-view':
+                new DBView($('#db-view')[0], this.db).render();
+                break;
+            case 'net-view':
+                new NetView($('#net-view'), this.net).render();
+                break;
+            case 'match-view':
+                new MatchView($('#match-view'), this.match).render();
+                break;
+            case 'profile-view':
+                new MeView($('#profile-view'), /*TODO*/null, /*TODO*/null, this.db).render();
+                break;
+            // case 'settings-view':
+            //     new SettingsView($('#settings-view'), this.store).render();
+            //     break;
         }
     }
 
     getAwareness(): Awareness {
         return this.net!.net.awareness;
     }
+
     mount(ele: HTMLElement) {
         $(ele).append(this.ele);
     }
@@ -155,5 +149,4 @@ export default class App {
     getObject(id: string): any {
         return this.db?.get(id);
     }
-
 }
